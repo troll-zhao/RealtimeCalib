cb1aea917ba82b4af65fe0944609ef08f0d2b21c
troll__zhao <troll__zhao@163.com>
2017-08-30 16:49:41 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/RealtimeCalib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RealtimeCalib.dir/main.cpp.o"
+  "RealtimeCalib.pdb"
+  "RealtimeCalib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RealtimeCalib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/RealtimeCalib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RealtimeCalib.
+# This may be replaced when dependencies are built.
